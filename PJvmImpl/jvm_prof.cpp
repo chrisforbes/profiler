2fab47d055889ca420707d1dbb5cd394234712f7
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-09-03 05:26:40 +0000

    wtfhax
    git-svn-id: svn://svn.ijw.co.nz/svn/Profiler@1505 993157c7-ee19-0410-b2c4-bb4e9862e678

--- PJvmImpl/jvm_prof.cpp
+++ PJvmImpl/jvm_prof.cpp
@@ -10,10 +10,49 @@
 //	$jdk/include/win32/
 //	$jdk/lib/
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <jvmti.h>
 #include <memory>
+#include <string>
+
+char const * txtProfileEnv = "ijwprof_txt";
+char const * binProfileEnv = "ijwprof_bin";
+
+std::string GetEnv( std::string const & name )
+{
+	static char buf[MAX_PATH];
+	GetEnvironmentVariableA( name.c_str(), buf, MAX_PATH );
+	return buf;
+}
 
 jvmtiEnv * jvmti = NULL;
+FILE * logfile = NULL;
+
+bool IsSystemMethod( std::string const & s )
+{
+	if (s.find("java.") == 0)
+		return true;
+	if (s.find("javax.") == 0)
+		return true;
+
+	return false;
+}
+
+void ExportMethodName( jvmtiEnv * ti_env, jmethodID method )
+{
+	char *method_name, clazz_name;
+	jclass clazz;
+
+	ti_env->GetMethodName( method, &method_name, NULL, NULL );
+	ti_env->GetMethodDeclaringClass( method, &clazz );
+	ti_env->GetClassSignature( clazz, (char **) &clazz_name, NULL );
+
+	fprintf( logfile, "0x%08x=%s::%s\n", method, clazz_name, method_name );
+
+	ti_env->Deallocate( (unsigned char *) clazz_name );
+	ti_env->Deallocate( (unsigned char *) method_name );
+}
 
 void JNICALL MethodEntry( jvmtiEnv * ti_env, JNIEnv * env, jthread thread, jmethodID method )
 {
@@ -26,9 +65,17 @@ void JNICALL MethodExit( jvmtiEnv * ti_env, JNIEnv * env, jthread thread, jmetho
 {
 	// todo: log method exit
 }
+void JNICALL JitMethod( jvmtiEnv * ti_env, jmethodID method, jint, void const *,
+					   jint, jvmtiAddrLocationMap const *, void const * )
+{
+	// todo: bind method name
+	ExportMethodName( ti_env, method );
+}
 
 JNIEXPORT jint JNICALL Agent_OnLoad( JavaVM * jvm, char * options, void * reserved )
 {
+	logfile = fopen( GetEnv( txtProfileEnv ).c_str(), "w" );
+
 	jvm->GetEnv( (void **) &jvmti, JVMTI_VERSION_1_0 );
 
 	jvmtiEventCallbacks callbacks;
@@ -36,11 +83,13 @@ JNIEXPORT jint JNICALL Agent_OnLoad( JavaVM * jvm, char * options, void * reserv
 
 	callbacks.MethodEntry = MethodEntry;
 	callbacks.MethodExit = MethodExit;
+	callbacks.CompiledMethodLoad = JitMethod;
 
 	jvmti->SetEventCallbacks( &callbacks, sizeof(jvmtiEventCallbacks) );
 	
 	jvmti->SetEventNotificationMode( JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL );
 	jvmti->SetEventNotificationMode( JVMTI_ENABLE, JVMTI_EVENT_METHOD_EXIT, NULL );
+	jvmti->SetEventNotificationMode( JVMTI_ENABLE, JVMTI_EVENT_COMPILED_METHOD_LOAD, NULL );
 
 	return JNI_OK;
 }
