829e7d23bec7fb7f00fd94683f8e3d564fdbe792
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-09-30 11:54:49 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/Profiler@1701 993157c7-ee19-0410-b2c4-bb4e9862e678

--- Profiler.Agents.JVM/JvmAgent.cs
+++ Profiler.Agents.JVM/JvmAgent.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Ijw.Profiler.Core;
 
-namespace Profiler.Agents.JVM
+namespace Ijw.Profiler.Agents.JVM
 {
-	public class JvmAgent
+	public class JvmAgent : IAgent
 	{
+		public Run Execute(RunParameters p)
+		{
+			throw new NotImplementedException();
+		}
+
+		public INameFactory NameFactory { get { throw new NotImplementedException(); } }
+
+		public string Name { get { return "Java"; } }
 	}
 }
